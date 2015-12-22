f3e1ca5a6c7528a65a8124d931d7b8006c9ba140
yuanxiang <yuanxiangxie@outlook.com>
2015-12-22 22:52:12 +0800

    cmomit

--- shell.sh
+++ shell.sh
@@ -2,7 +2,7 @@
 spawn ./git.sh
 expect "Username for 'https://github.com': " 
 send "yuanxiangxie@outlook.com\n"
-expect "Password for 'https://yuanxiangxie@outlook.com@github.com': "
+expect "Password for 'https://yuanxiangxie@outlook.com@github.com':"
 send "xyx19950301\n"
 
 

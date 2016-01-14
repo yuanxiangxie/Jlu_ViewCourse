a6603dc46930faf1cbb5b1b67e89dd68c0048338
yuanxiang <yuanxiangxie@outlook.com>
2016-01-14 12:47:19 +0800

    jlu select_course.py

--- shell.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/expect
-spawn ./git.sh
-expect "Username for 'https://github.com': " 
-send "yuanxiangxie@outlook.com\n"
-expect "Password for 'https://yuanxiangxie@outlook.com@github.com':"
-send "xyx19950301\n"
-
-
-
